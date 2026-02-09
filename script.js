3d3c64ed1a2a0091df8f138e65d659881e17567d
otman5000-sketch <otman5000@gmail.com>
2026-02-10 00:22:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,4 +7,26 @@ function pick(button) {
   const final = document.getElementById("finalAnswer");
   final.textContent = "Honestly? All of the above 🙂";
   final.style.opacity = 1;
+
+  document.getElementById("continueBtn").style.opacity = 1;
+}
+
+function continueStory() {
+  document.getElementById("nextSection").scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+function finalPick(choice) {
+  const msg = document.getElementById("finalMessage");
+
+  if (choice === "yes") {
+    msg.textContent =
+      "I like that 🙂\nNo pressure. Just two people enjoying the moment.";
+  } else {
+    msg.textContent =
+      "That’s okay too 😌\nGood things don’t need to be rushed.";
+  }
+
+  msg.style.opacity = 1;
 }
