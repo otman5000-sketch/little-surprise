418f9b8da7a6bb8e8f5a39bcecaae3bcf8926181
otman5000-sketch <otman5000@gmail.com>
2026-02-10 16:17:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -54,8 +54,3 @@ function chooseNo() {
     "That’s okay. Some things are powerful enough just to be seen.";
   result.style.opacity = 1;
 }
-function showLastStory() {
-  const last = document.getElementById("lastStory");
-  last.style.display = "block";
-  last.scrollIntoView({ behavior: "smooth" });
-}
